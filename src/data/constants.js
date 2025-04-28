633ecdd6253cfdfe739dfb535e62e73e6c5018c1
Sudhirarya <sudhirarya7033@gmail.com>
2025-04-29 03:07:29 +0530

    resume update

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Sudhirarya",
   resume:
-    "https://drive.google.com/file/d/1aNIl6y4E--urk-lfyT_ROdGBuoOYpk3j/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1yIZ8MJz7Y0XXl9SrQe46MzSOuZq-aJ20/view?usp=drivesdk",
   linkedin:
     "https://www.linkedin.com/in/sudhir-kumar7033/?originalSubdomain=in/",
   twitter: "https://twitter.com/sudhir703375",
